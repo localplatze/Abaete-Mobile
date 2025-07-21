d7b881e4c4ddf17bdb7ae40a9be33eac9ab51d5e
localplatze <platze8@gmail.com>
2025-07-21 10:17:00 -0400

    vercel

--- App.js
+++ App.js
@@ -28,32 +28,7 @@ export default function App() {
   useEffect(() => {
     loadFonts().then(() => setFontsLoaded(true));
   }, []);
-
-  useEffect(() => {
-    const setupNotifications = async () => {
-      try {
-        const token = await registerForPushNotifications();
-        
-        if (token) {
-          console.log('App registrado para notificações');
-        }
-      } catch (error) {
-        console.error('Erro ao configurar notificações:', error);
-      }
-    };
-
-    const notificationListener = listenToNotifications(notification => {
-      console.log('Notificação recebida:', notification);
-      // Aqui você pode adicionar lógica para tratar a notificação
-    });
-
-    setupNotifications();
-
-    return () => {
-      notificationListener.remove();
-    };
-  }, []);
-
+  
   return (
     <NavigationContainer>
       <Stack.Navigator
